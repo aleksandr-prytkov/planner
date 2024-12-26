372a4c09fc6fb0ac5b30e0dbaf64006a0f1fd573
Aleksandr Prytkov <alexandr.prytkow@yandex.ru>
2024-12-26 19:31:42 +0300

    add event model

---
+++ models/events.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel
+from typing import List
+
+
+class Event(BaseModel):
+    id: int
+    title: str
+    image: str
+    description: str
+    tags: List[str]
+    location: str
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "My First Event",
+                "image": "https://myimage.com/picture.png",
+                "description": "My description for my first event",
+                "tags": ["python", "fastapi", "launch"],
+                "location": "Google meeting"
+            }
+        }
